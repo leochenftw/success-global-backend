24fe90cd9d03f8baabf012abc0e5f427317ba874
Leo Chen <leo@saltedherring.com>
2018-07-05 00:50:36 -1200

    retarded...

--- themes/default/js/custom.scripts.js
+++ themes/default/js/custom.scripts.js
@@ -2,6 +2,5 @@ var pathname            =   window.location.pathname;
 
 if (pathname.indexOf('/Security') != 0 && pathname.indexOf('/dev') != 0) {
     var target_url      =   '/!/#' + window.location.pathname;
-    console.log(target_url);
     window.location.replace(target_url);
 }
